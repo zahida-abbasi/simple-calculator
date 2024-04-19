7a3a44d6ae53548f8eab3ebbeba91dd05248f178
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-04-19 15:22:28 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple Calculator
+I am a programmer.I made this simple calculator professionally to calculate numbers with basic data type operators.I hope it will be helpful for you.Thanks.
---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    { message: "Select one of the operators to perform operation", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"] }
+]);
+console.log(answer);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    ("please  enter valid operator");
+}
